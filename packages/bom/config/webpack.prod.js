529270aa6aecb33aa40e9154df93fec46d31546d
张思旺 <Zhangsw02@mingyuanyun.com>
2018-09-11 10:32:54 +0800

    init code

--- packages/bom/config/webpack.prod.js
+++ packages/bom/config/webpack.prod.js
@@ -1,86 +1,101 @@
 const paths = require('./paths')
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-const cssFilename = 'static/css/[name].[contenthash:8].css';
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
   mode: 'production',
   entry: paths.componentsEntry,
   output: {
     path: paths.output,
-    libraryTarget: "commonjs2",
-    filename: '[name]/index.js',
+    libraryTarget: 'commonjs2',
+    filename: '[name]/index.js'
   },
   devtool: 'source-map',
   resolve: {
-    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx']
+    extensions: [
+      '.web.js',
+      '.mjs',
+      '.js',
+      '.json',
+      '.web.jsx',
+      '.jsx'
+    ]
   },
   module: {
     rules: [
       {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: require.resolve('url-loader'),
-        options: {
-          limit: 10000,
-          name: 'static/media/[name].[hash:8].[ext]',
-        },
-      },
-      {
-        test: /\.(js|jsx|mjs)$/,
-        // include: paths.componentsEntry,
-        loader: require.resolve('babel-loader'),
-        options: {
-          compact: true,
-        },
-      },
-      {
-        test: /\.css$|\.less$/,
-        loader: ExtractTextPlugin.extract(
-          Object.assign(
-            {
-              fallback: {
-                loader: require.resolve('style-loader'),
+        oneOf: [
+          {
+            test: [
+              /\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/
+            ],
+            loader: require.resolve('url-loader'),
+            options: {
+              limit: 10000,
+              name: 'static/media/[name].[hash:8].[ext]'
+            }
+          }, {
+            test: /\.(js|jsx|mjs)$/,
+            // include: paths.componentsEntry,
+            loader: require.resolve('babel-loader'),
+            options: {
+              compact: true
+            }
+          }, {
+            test: /\.css$|\.less$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+              }, {
+                loader: require.resolve('css-loader'),
                 options: {
-                  hmr: false,
-                },
-              },
-              use: [
-                {
-                  loader: require.resolve('css-loader'),
-                  options: {
-                    importLoaders: 1,
-                    minimize: true,
-                    sourceMap: true,
-                  },
-                },
-                {
-                  loader:'less-loader',
-                  options: {
-                    // less 变量注入
-                    modifyVars: {}
-                  }
+                  importLoaders: 1,
+                  minimize: true,
+                  sourceMap: true
                 }
-              ]
+              }, {
+                loader: 'less-loader',
+                options: {
+                  // less 变量注入
+                  modifyVars: {}
+                }
+              }
+            ]
+          }, {
+            loader: require.resolve('file-loader'),
+            exclude: [
+              /\.(js|jsx|mjs)$/, /\.html$/, /\.json$/
+            ],
+            options: {
+              name: 'static/media/[name].[hash:8].[ext]'
             },
-          )
-        ),
-        // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
-      },
-
-      {
-        loader: require.resolve('file-loader'),
-        exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
-        options: {
-          name: 'static/media/[name].[hash:8].[ext]',
-        },
-        // ** STOP ** Are you adding a new loader?
-        // Make sure to add the new loader(s) before the "file" loader.
+            // ** STOP ** Are you adding a new loader? Make sure to add the new loader(s)
+            // before the 'file' loader.
+          }
+        ]
       }
     ]
   },
   plugins: [
-    new ExtractTextPlugin({
-      filename: cssFilename,
-    }),
-  ]
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output both options are
+      // optional
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true // set to true if you want JS source maps
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  performance: {
+    hints: 'warning'
+  }
 }
--- packages/bom/scripts/build.js
+++ packages/bom/scripts/build.js
@@ -2,10 +2,16 @@ const webpack = require('webpack')
 const path = require('path')
 const webpackConfig = require('../config/webpack.prod')
 const util = require('util')
+const fs = require('fs-extra')
+const paths = require('../config/paths')
+const ProgressPlugin = require('webpack/lib/ProgressPlugin')
 
-async function build () {
+function build () {
+  // 清空输出目录
+  fs.emptyDirSync(paths.output)
+
+  // 运行webpack构建
   const compiler = webpack(webpackConfig)
-
   compiler.run((error, stats) => {
     console.log(error, stats)
   })
